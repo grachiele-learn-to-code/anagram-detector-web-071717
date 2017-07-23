09b0caa385dafecaee1ff6a4e83d7cce0188475a
Gianpaul Rachiele <gianpaul817@gmail.com>
2017-07-23 09:52:03 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(words)
+    matches = []
+    words.each do |item|
+      if @word.chars.sort == item.chars.sort
+        matches << item
+      end
+    end
+    matches
+  end
+
+
+end
